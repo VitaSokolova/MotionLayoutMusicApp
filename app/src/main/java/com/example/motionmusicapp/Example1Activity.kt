1894edc257f14a91e71e53a47d06c24dfae991ce
sokolova <sokolova@surfstudio.co>
2020-02-19 18:56:20 +0300

    basic MotionLayout examples

--- app/src/main/java/com/example/motionmusicapp/Example1Activity.kt
+++ app/src/main/java/com/example/motionmusicapp/Example1Activity.kt
@@ -1,15 +1,12 @@
-
 package com.example.motionmusicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.scene2.*
 
-class MainActivity : AppCompatActivity() {
+class Example1Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_example_1)
     }
 }
---
+++ app/src/main/java/com/example/motionmusicapp/Example2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.motionmusicapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Example2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_2)
+    }
+}
